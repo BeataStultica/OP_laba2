a33a10cb2114fd8ce96ccf86b7604f0dfc665358
Eduard <gawcutya17@gmail.com>
2020-03-04 21:52:30 +0200

    Переделал сортировку,добавил функцию заменяющую голоса сразу на количество баллов

--- sorter.py
+++ sorter.py
@@ -1,7 +1,21 @@
-def sorter(country_list):
-	result = []
-	for fst in country_list:
-		sortd = sorted(fst, key=lambda x: int(x[1].strip()))
-		result.append(sortd)
-	return result
+def sort2(i):
+	return i[1]
+
+
+def ball(score):
+	for i in range(len(score)):
+		score[i].sort(key=sort2,reverse=True)
+		for j in range(len(score[i])):
+			if j == 0:
+				score[i][j][1] = 12
+			elif i == 1:
+				score[i][j][1] = 10
+			elif j<10:
+				score[i][j][1] = 10-j
+			else:
+				score[i][j][1] = 0
+	return score
+
+
+
 
