ca0adbfd4a8aaab5e109dc659c0eea783a913eea
Dmytro <dimon2000.1982@gmail.com>
2020-03-05 00:31:03 +0200

    добавил функцию main, придал программе финальный вид

--- opener.py
+++ opener.py
@@ -1,6 +1,5 @@
 import os
-from sorter import sorter
-
+from sorter import *
 def reader():
 	teca = input("Введіть назву теки:")
 	all_info = []
@@ -14,6 +13,7 @@ def reader():
 				impermanent_mass.append(line)
 		all_info += impermanent_mass[1::]
 		amount_country += int(impermanent_mass[0])
+	file1.close()
 	return(all_info, amount_country)
 
 
@@ -23,15 +23,15 @@ def column_score(all_info, amount_country):
 		fx = []
 		for j in range(amount_country):
 			country_info = all_info[j].split(",")
-			fx.append([country_info[0],country_info[i+1]])
+			fx.append([country_info[0],int(country_info[i+1])])
 		column.append(fx)
-	print(column)
-	return column
 
+	return(column)
 
-if __name__ == "__main__":
-	a = reader()
-	info = column_score(a[0], a[1])
-	res = sorter(info)
-	print(res)
+def main():
+	info_list = reader()
+	column_s = column_score(info_list[0], info_list[1])
+	country_ball = ball(column_s)
+	finish(country_ball)
 
+main()
--- sorter.py
+++ sorter.py
@@ -1,33 +1,32 @@
 def sort2(i):
 	return i[1]
 
-
 def ball(score):
 	for i in range(len(score)):
-		score[i].sort(key=sort2,reverse=True)
+		score[i].sort(key = sort2,reverse=True)
 		for j in range(len(score[i])):
 			if j == 0:
 				score[i][j][1] = 12
-			elif i == 1:
+			elif j == 1:
 				score[i][j][1] = 10
-			elif j<10:
+			elif j < 10:
 				score[i][j][1] = 10-j
 			else:
 				score[i][j][1] = 0
 	return score
 
 def finish(score):
-	b=[]
+	b = []
 	for t in range(len(score)):
 		score[t].sort()
 	for i in range(1,len(score)):
 		for j in range(len(score)):
 			score[0][j][1] += score[i][j][1]
-	score[0].sort(key = sort2,reverse = True)
+	score[0].sort(key = sort2,reverse=True)
 	txt = open("result.txt",'w')
 	for k in range(10):
-		txt.write(str(score[0][k][0] + ',' + str(score[0][k][1]) + '\n\)
+		txt.write(str(score[0][k][0]) + ',' + str(score[0][k][1])+'\n')
+	txt.close()
 	print("\n")
 	print(score[0])
-
 
