6c25ea82beb32abe192c4a6345e086de1d55879c
Eduard <gawcutya17@gmail.com>
2020-03-04 22:59:27 +0200

    добавил функцию которая суммирует баллы каждой страны,записывает их в первую колонку,сортиует по убыванию и записывает первые 10 стран в файл

--- sorter.py
+++ sorter.py
@@ -16,6 +16,18 @@ def ball(score):
 				score[i][j][1] = 0
 	return score
 
-
+def finish(score):
+	b=[]
+	for t in range(len(score)):
+		score[t].sort()
+	for i in range(1,len(score)):
+		for j in range(len(score)):
+			score[0][j][1] += score[i][j][1]
+	score[0].sort(key = sort2,reverse = True)
+	txt = open("result.txt",'w')
+	for k in range(10):
+		txt.write(str(score[0][k][0] + ',' + str(score[0][k][1]) + '\n\)
+	print("\n")
+	print(score[0])
 
 
